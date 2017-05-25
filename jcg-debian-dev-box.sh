83e8d5da09326018d1208d09e219826e1b977ce8
John Griffin <griffinish@gmail.com>
2017-05-25 16:00:43 -0400

    Update jcg-debian-dev-box.sh

--- jcg-debian-dev-box.sh
+++ jcg-debian-dev-box.sh
@@ -30,11 +30,63 @@ apt-get ${APTOPT} install \
 
 curl -s 'https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz' | tar -C /usr/local -xzf -
 
-updatedb
+cat <<__EOF__  >> /etc/skel/.emacs
+
+; General
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+(setq make-backup-files nil)
+(menu-bar-mode -1)
+
+(setq-default tab-width 4)
+
+(set-face-foreground 'font-lock-comment-face "color-240")
+
+(custom-set-faces
+ '(flymake-errline ((((class color)) (:foreground "White" :background "Red"))))
+ '(flymake-warnline ((((class color)) (:underline "Yellow")))))
+
+(load-theme 'wheatgrass)
+
+
+; Python
+(setq python-shell-interpreter "python3")
+
+; Racket
+(add-to-list 'auto-mode-alist '("\\.rkt" . scheme-mode))
+
+
+__EOF__
+
+
+cat <<__EOF__ >> /etc/skel/.vimrc
+
+syntax on
+set background=dark
+let loaded_matchparen = 1
+
+; Python-happy
+:set ts=8 et sw=4 sts=4
+
+set foldmethod=indent
+set foldlevel=99
+
+
+__EOF__
 
 
 echo '
 export PATH=$PATH:/usr/local/go/bin
 ' >> /etc/profile.d/path.sh
+
+
+updatedb
+
+
 
 
