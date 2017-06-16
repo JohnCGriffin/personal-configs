bf666f93aa893fa24fd1be21d9c61911cf2bef8b
John Griffin <griffinish@gmail.com>
2017-06-16 19:13:14 -0400

    Create GoCommands.sh

---
+++ GoCommands.sh
@@ -0,0 +1,5 @@
+go get -u golang.org/x/tools/cmd/...
+go get -u github.com/regpeppe/godef
+go get -u github.com/nsf/gocode
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/guru
