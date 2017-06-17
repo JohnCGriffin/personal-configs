03729487e9fa48ad68fa24b3fed23f9532069db2
John Griffin <griffinish@gmail.com>
2017-06-16 20:17:28 -0400

    Update GoCommands.sh

--- GoCommands.sh
+++ GoCommands.sh
@@ -1,5 +1,5 @@
-go get -u golang.org/x/tools/cmd/...
-go get -u github.com/regpeppe/godef
-go get -u github.com/nsf/gocode
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/guru
+go get golang.org/x/tools/cmd/...
+go get github.com/regpeppe/godef
+go get github.com/nsf/gocode
+go get golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/guru
