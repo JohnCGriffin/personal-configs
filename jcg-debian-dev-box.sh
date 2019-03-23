cf637c235dac468a67daf65ae3c24c55dc17bbeb
John Griffin <griffinish@gmail.com>
2019-03-23 13:20:55 -0400

    Update jcg-debian-dev-box.sh
    pip3 installs

--- jcg-debian-dev-box.sh
+++ jcg-debian-dev-box.sh
@@ -32,6 +32,8 @@ apt-get ${APTOPT} install \
 
 updatedb
 
+pip3 install setuptools && pip3 install ipython
+
 curl -s 'https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz' | tar -C /usr/local -xzf -
 
 THEMES=`locate wheatgrass-theme.el | sort | grep emacs/2 | tail -1 | sed -e 's/wheatgrass-theme.el//'`
