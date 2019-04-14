5a3d9f049e5364df13149d9056ab3b9f135dfba8
John Griffin <griffinish@gmail.com>
2019-04-14 19:33:21 -0400

    fixed single quote

--- jcg-debian-dev-box.sh
+++ jcg-debian-dev-box.sh
@@ -53,7 +53,7 @@ export PATH=$PATH:/usr/local/go/bin
 ' >> /etc/profile.d/path.sh
 
 cat << __EOS__ >> /etc/skel/.profile
-export PS1=${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 export EDITOR=emacs
 export PATH=$PATH:/usr/local/go/bin
 __EOS__
