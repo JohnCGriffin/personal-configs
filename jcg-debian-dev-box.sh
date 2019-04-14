48a79a348a33593df4de76aabad8fa7d27b9f407
John Griffin <griffinish@gmail.com>
2019-04-14 19:27:49 -0400

    Update jcg-debian-dev-box.sh

--- jcg-debian-dev-box.sh
+++ jcg-debian-dev-box.sh
@@ -52,11 +52,11 @@ echo '
 export PATH=$PATH:/usr/local/go/bin
 ' >> /etc/profile.d/path.sh
 
-echo '
-export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+cat << __EOS__ >> /etc/skel/.profile
+export PS1=${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 export EDITOR=emacs
 export PATH=$PATH:/usr/local/go/bin
-' >> /etc/skel/.profile
+__EOS__
 
 
 updatedb
