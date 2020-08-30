67e9b24f630a32d711c04b5523a5c6d77e14538d
John Griffin <griffinish@gmail.com>
2020-08-30 08:46:28 -0400

    local init.el setup, added clang

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM debian:buster
+FROM ubuntu:20.04
 
 MAINTAINER griffinish@gmail.com
 
@@ -13,7 +13,7 @@ RUN apt-get clean
 RUN apt-get -qq update 
 RUN apt-get upgrade -y 2> /dev/null < /dev/null
 RUN apt-get -qq install --no-install-recommends -y apt-utils 2> /dev/null < /dev/null
-RUN apt-get -qq install nodejs npm sudo  -y apt-utils 2> /dev/null < /dev/null
+RUN apt-get -qq install -y nodejs npm sudo apt-utils 2> /dev/null < /dev/null
 RUN apt-get -qq -y install \
 	build-essential cmake git \
 	rustc \
@@ -23,12 +23,13 @@ RUN apt-get -qq -y install \
 	man \
 	python3 python3-pip ipython3 \
 	wget curl
+RUN apt-get -qq -y install clang
 
 
 
 RUN echo 'griffin ALL=(root)NOPASSWD: ALL' > /etc/sudoers.d/griffin
 
-RUN git clone https://github.com/JohnCGriffin/dot-emacs-dot-d.git /etc/skel/.emacs.d
+COPY init.el /etc/skel/.emacs.d/
 
 RUN sed -i -e "s/;34m/;37m/" /etc/skel/.bashrc
 
@@ -43,6 +44,8 @@ COPY sshd_config.to_append.txt /tmp
 RUN cat /etc/ssh/ssh_config /tmp/sshd_config.to_append.txt > /tmp/ssh_config && cp /tmp/ssh_config /etc/ssh
 
 RUN yes | adduser griffin > /dev/null 2>&1
+
+RUN su - griffin -c "emacs --script ~griffin/.emacs.d/init.el"
 
 USER griffin
 
---
+++ init.el
@@ -0,0 +1,69 @@
+
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(setq package-check-signature nil)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Conditional installations
+
+
+(defmacro conditional-install (name)
+  `(unless (package-installed-p (quote ,name))
+     (package-refresh-contents)
+     (package-install (quote ,name))))
+
+(progn
+  (unless (version<= emacs-version "25.3") ; as in Centos 7
+    (conditional-install magit)
+    (conditional-install markdown-mode))
+  ;(conditional-install clojure-mode)
+  ;(conditional-install cider)
+  ;(conditional-install lsp-mode)
+  (conditional-install go-mode)
+  (conditional-install flycheck)
+  (conditional-install rust-mode)
+  (conditional-install dockerfile-mode)
+  ;(conditional-install lsp-python-ms)
+  (conditional-install company))
+
+(progn
+  (add-hook 'c++-mode-hook (lambda () (flycheck-mode))))
+
+;; (defun lsp-go-install-save-hooks ()
+;;   (add-hook 'before-save-hook #'lsp-format-buffer t t)
+;;   (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
+;; (progn
+
+;;   (add-hook 'go-mode-hook 'lsp)
+;;   (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
+
+
+;; Basics
+
+(progn
+
+  (menu-bar-mode -1)
+  (add-to-list 'load-path "~/.emacs.d/lisp/")
+  (load-theme 'wombat t)
+
+  (global-set-key (kbd "C-x g") 'magit-status)
+
+  (fset 'yes-or-no-p 'y-or-n-p)
+
+  (setq make-backup-files nil
+	auto-save-default nil
+	inhibit-startup-screen t
+	ring-bell-function 'ignore
+	tab-width 4
+	c-base-indent 4
+	c-basic-offset 4
+	python-indent-offset 4
+	python-indent-guess-indent-offset t
+	python-indent-guess-indent-offset-verbose nil
+	indent-tabs-mode nil))
+
