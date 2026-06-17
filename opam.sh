2c2d375229146460291aa3315579cd5c81ce53cb
John Griffin <griffinish@gmail.com>
2026-06-17 08:17:26 -0400

    more opam fixes

--- opam.sh
+++ opam.sh
@@ -3,7 +3,12 @@
 
 echo . ~/.opam/opam-init/init.sh >> ~/.bashrc
 
+opam switch create jcgdebian 5.3.0
+opam switch set jcgdebian
+
 opam install -y dune
 opam install -y bitv
 opam install -y curses
+opam install -y ocamlformat
+opam install -y ocaml-lsp-server
 
